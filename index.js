97337e7f44116237db4a209f937bb78eea2d7f18
Larissa Farias <larissa.farias@ngi.com.br>
2017-06-06 11:23:26 -0300

    migration

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const { create } = require('rung-sdk');
-const { OneOf, Money } = require('rung-sdk/dist/types');
-const Bluebird = require('bluebird');
-const agent = require('superagent');
-const promisifyAgent = require('superagent-promise');
-const { path, lt, gt } = require('ramda');
+import { create } from 'rung-sdk';
+import { OneOf, Money } from 'rung-sdk/dist/types';
+import Bluebird from 'bluebird';
+import agent from 'superagent';
+import promisifyAgent from 'superagent-promise';
+import { path, lt, gt } from 'ramda';
 
 const request = promisifyAgent(agent, Bluebird);
 
+function renderAlert(origin, target, result) {
+    return [{
+        title: `${origin} ${_('worth')} ${target} ${result}`,
+        content: `${origin} ${_('worth')} ${target} ${result}`,
+        comment: `${origin} ${_('worth')} ${target} ${result}`
+    }];
+}
+
 function main(context, done) {
     const { origin, target, comparator, value } = context.params;
-    const compare = comparator === 'maior' ? gt : lt;
+    const compare = comparator === 'maior que' ? gt : lt;
 
     if (origin === target) {
         return done({});
@@ -18,10 +26,10 @@ function main(context, done) {
     return request.get(`http://api.fixer.io/latest?base=${origin}`)
         .then(path(['body', 'rates', target]))
         .then(result => compare(result, value)
-            ? [`${origin} valendo ${target} ${result.toFixed(2)}`]
-            : {})
+            ? { alerts: renderAlert(origin, target, result.toFixed(2)) }
+            : { alerts: {} })
         .then(done)
-        .catch(() => done({}));
+        .catch(() => done({ alerts: {} }));
 }
 
 const currencies = [
@@ -33,27 +41,25 @@ const currencies = [
 
 const params = {
     origin: {
-        description: 'Moeda de origem',
+        description: _('Currency of origin'),
         type: OneOf(currencies),
         default: 'USD'
     },
     target: {
-        description: 'Moeda de destino',
+        description: _('Currency of destination'),
         type: OneOf(currencies),
         default: 'BRL'
     },
     comparator: {
-        description: 'Tipo de comparação',
-        type: OneOf(['maior', 'menor']),
-        default: 'menor'
+        description: _('Comparison type'),
+        type: OneOf(['maior que', 'menor que']),
+        default: 'menor que'
     },
     value: {
-        description: 'Valor de comparação',
+        description: _('Comparison value'),
         type: Money,
         default: 3.0
     }
 };
 
-const app = create(main, { params });
-
-module.exports = app;
+export default create(main, { params });
